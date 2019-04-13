b715f808f1b34d32405a25fe4f6b58024b632d17
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2019-04-13 03:12:50 -0300

    initial commit

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# chromakey
+Studying canvas and [Video Manipulation](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Manipulating_video_using_canvas) to create a chromakey effect on web.
--- src/index.js
+++ src/index.js
@@ -1,63 +1,87 @@
-import { loadavg } from "os";
+import videoSrc from './171003D_017_2K.mp4'
 
-const url =
-  "https://www.videvo.net/videvo_files/converted/2015_01/preview/AMS__Big_Explosion.mov67018.webm";
+const maxW = 1280
+const maxH = 960
 
-const processor = function processor(src) {
-  const video = document.createElement("video");
-  const c1 = document.createElement("canvas");
-  const ctx1 = c1.getContext("2d");
-  const c2 = document.createElement("canvas");
-  const ctx2 = c2.getContext("2d");
+const chromakey = function chromakey(src) {
+  const video = document.createElement('video')
+  const canvasContainer = document.createElement('div')
+  const c1 = document.createElement('canvas')
+  const ctx1 = c1.getContext('2d')
+  const c2 = document.createElement('canvas')
+  const ctx2 = c2.getContext('2d')
 
   let width = 0,
-    height = 0;
-  // const self = this
+    height = 0
 
   const load = () => {
-    video.src = src;
-    video.controls = true;
+    appendVideo()
 
-    video.addEventListener("play", ev => {
-      width = video.width / 2;
-      height = video.height / 2;
+    video.addEventListener('play', ev => {
+      appendCanvas()
+      loop()
+    })
 
-      c1.id = "c1";
-      c1.width = width;
-      c1.height = height;
-
-      c1.id = "c2";
-      c2.width = width;
-      c2.height = height;
-
-      loop();
-    });
-
-    document.body.appendChild(video);
-    document.body.appendChild(c1);
-    document.body.appendChild(c2);
-  };
+  }
 
   const loop = () => {
-    console.log("looping");
-    if (video.paused || video.ended) return;
-    // computeFrame();
-    window.requestAnimationFrame(loop);
-  };
+    if (video.paused || video.ended) return
+    computeFrame()
+    window.requestAnimationFrame(loop)
+  }
 
   const computeFrame = () => {
-    ctx1.drawImage(video, 0, 0, width, height);
-    const frame = ctx1.getImageData(0, 0, width, height);
-    const l = frame.data.length / 4;
+    ctx1.drawImage(video, 0, 0, width, height)
+    const frame = ctx1.getImageData(0, 0, width, height)
+    const l = frame.data.length / 4
 
     for (let i = 0; i < l; i++) {
       let r = frame.data[i * 4 + 0];
       let g = frame.data[i * 4 + 1];
       let b = frame.data[i * 4 + 2];
-      if (g > 100 && r <= 10 && b <= 10) frame.data[i * 4 + 3] = 0;
+      if (r < 120 && g > 100 && b < 80) {
+        frame.data[i * 4 + 3] = 0;
+      }
     }
-  };
-  load();
-};
 
-document.addEventListener("DOMContentLoaded", () => processor(url));
+    ctx2.putImageData(frame, 0, 0);
+  }
+
+  const appendVideo = () => {
+    document.body.appendChild(video)
+    window.requestAnimationFrame(() => configVideo())
+  }
+
+  const configVideo = () => {
+    video.src = src
+    video.controls = true
+
+    let { width: vw, height: vh } = video.getBoundingClientRect()
+
+    vw = vw > maxW ? maxW : vw
+    vh = vh > maxH ? maxH : vw
+
+    width = vw / 2
+    height = vh / 2
+
+    video.setAttribute('width', vw)
+    video.setAttribute('height', vh)
+  }
+
+  const appendCanvas = () => {
+    document.body.appendChild(c1)
+    document.body.appendChild(c2)
+    configCanvas()
+  }
+
+  const configCanvas = () => {
+    [c1, c2].forEach((c) => {
+      c.width = width
+      c.height = height
+      })
+  }
+
+  load()
+}
+
+document.addEventListener('DOMContentLoaded', () => chromakey(videoSrc))
